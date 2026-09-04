58b7f17ae7e54cc648ad6b8604995ed90f583e70
yEdsonJacovetti <edsonjacovetti2002@gmail.com>
2026-09-04 09:28:03 -0300

    Create CLUP.py

---
+++ CLUP.py
@@ -0,0 +1,14 @@
+nome_do_aluno = input("Nome do aluno(a): ")
+nome_da_disciplina = input("Nome da disciplina: ")
+turma = input("Digite seu período: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+media = (nota1 + nota2 + nota3) / 3
+print('Aluno(a):', nome_do_aluno)
+print('Disciplina:', nome_da_disciplina)
+print('nota 1:', nota1,'nota 2:', nota2,'nota 3:', nota3)
+print('Turma:', turma)
+print('Média Final:', media)
+
+#Feito no vscode mas abra no idle/pycharm para melhor visualização do código.
